6202df4c73effb3a22f4932639afb989c149843a
Nahian-Alvy <117886480+Nahian-Alvy@users.noreply.github.com>
2023-10-05 21:42:04 +0600

    Add files via upload

---
+++ firstday.py
@@ -0,0 +1,11 @@
+print(type("hello"))
+print(type(6))
+print(8**2) #power
+print(6%4)
+print(round(4.8))
+print(abs(-30))
+a,b,c =1,3,4    #it'll print 1,2,3 sequence way
+print(a,b,c)
+PI =3.14 #Constraitns which can't be change 
+print(PI)
+print(bin(10)) #it'll print binary value into integer
